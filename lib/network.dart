e06029c1343820312e8b6a1c175ebcc46f1d6919
nitin <3nitinkaushik@gmail.com>
2023-08-03 19:17:23 +0530

    web sopport added for geolocation

--- lib/network.dart
+++ lib/network.dart
@@ -5,7 +5,7 @@ class network {
   dynamic temp;
   Future gettempbylocation({required double long,required double lat}) async{
 
-    var url=Uri.http('api.openweathermap.org','/data/2.5/weather',{'lon':'$long','lat':'$lat','units':'metric','appid':'6a1efbda172d2c1f6cfa96e94bd0fded'});
+    var url=Uri.https('api.openweathermap.org','/data/2.5/weather',{'lon':'$long','lat':'$lat','units':'metric','appid':'6a1efbda172d2c1f6cfa96e94bd0fded'});
     var response =await http.get(url);
     if (response.statusCode == 200) {
       var jsonResponse = convert.jsonDecode(response.body) ;
